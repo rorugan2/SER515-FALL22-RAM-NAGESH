8777ec4e51663cf24bc6ac874beb004069ce6bce
rorugan2 <rorugan2@DESKTOP-M7P270P>
2022-10-20 16:56:06 -0700

    produce product menu displayed

--- ProduceProductMenu.java
+++ ProduceProductMenu.java
@@ -7,19 +7,34 @@ import java.util.StringTokenizer;
 public class ProduceProductMenu implements ProductMenu {
 
 	public void showMenu(){
-		try {
-			FileReader f = new FileReader("C:\\Users\\rorugan2\\Downloads\\515 files\\ProductInfo.txt");
-			BufferedReader br = new BufferedReader(f);
-			String s = br.readLine();
-			while (s!=null){
-				System.out.println(s);
-				s=br.readLine();
+		
+			try {
+				FileReader f = new FileReader("C:\\Users\\rorugan2\\Downloads\\515 files\\ProductInfo.txt");
+				BufferedReader br = new BufferedReader(f);
+				String s = br.readLine();
+				while (s!=null){
+					StringTokenizer st = new StringTokenizer(s,":");
+					while(st.hasMoreTokens()){
+						String product = st.nextToken();
+						String value = st.nextToken();
+						if(product.equals("Produce"))
+						{
+							
+							System.out.println(product +":"+value);
+						}
+						
+					}
+					
+					s=br.readLine();
+				}			
 			}			
-		}			
-		catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}	
+			catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}	
+		
+	
+			
 
 
 	}
